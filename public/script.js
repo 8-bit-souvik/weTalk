5cf49e1cfe212d0b8736d2c6cafc7e3a804c602e
souvik <souvikmandal210@gmail.com>
2022-01-24 22:56:56 +0530

    lets find bug on production ;-]

--- public/script.js
+++ public/script.js
@@ -9,3 +9,19 @@ document.querySelector(".collapsible").addEventListener("click", function () {
     }
 })
 
+
+document.querySelector(".sub-navs").addEventListener("click", function () {
+    var navbar = document.querySelector(".menu");
+    if (navbar.style.left == '0px') {
+        navbar.style.left = '-225px'
+        navbar.querySelector("#arrow").src = "public/icons/angle-right-solid.svg"
+    } else {
+        navbar.style.left = '0px'
+        navbar.querySelector("#arrow").src = "public/icons/angle-left-solid.svg"
+    }
+})
+
+
+document.querySelector(".logoimage").addEventListener("click", function () {
+window.location.href = "/home"
+})
